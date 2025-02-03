bdca1165e17d14b3f95eb6c6a1572176bfe79f07
Maxim280596 <max280596@gmail.com>
2025-02-03 14:03:29 +0200

    initial commit

---
+++ scripts/deploy.ts
@@ -0,0 +1,10 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  console.log("Deploying contracts...");
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
